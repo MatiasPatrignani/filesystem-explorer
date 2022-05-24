e5897a7c776f2dc0d0ca0558d1fa096716f68d86
Walber Melo <dev4wm@gmail.com>
2022-05-24 16:41:12 +0100

    feat(upload):implement upload method

---
+++ modules/upload.php
@@ -0,0 +1,30 @@
+<?php
+
+if (isset($_POST['submit'])) {
+  $file = $_FILES['add_file'];
+}
+
+$fileName = $_FILES['add_file']['name'];
+$fileTmpName = $_FILES['add_file']['tmp_name'];
+$fileSize = $_FILES['add_file']['size'];
+$fileError = $_FILES['add_file']['error'];
+$fileType = $_FILES['add_file']['type'];
+
+$fileExt = explode('.', $fileName); 
+$fileActualExt = strtolower((end($fileExt))); 
+
+$typesAllowed = array('doc', 'csv', 'jpg', 'png', 'txt', 'ppt', 'odt', 'pdf', 'zip', 'rar', 'exe', 'svg', 'mp3', 'mp4');
+
+if(in_array($fileActualExt,$typesAllowed,true)) {
+  if($fileError === 0) {
+    if($fileSize < 500000) {
+      $fileNameNew = uniqid("", true) . "." . $fileActualExt;
+      $fileDestination = "/root/$fileNameNew";
+      echo $fileDestination;
+      move_uploaded_file($fileTmpName, $fileDestination);
+    }
+  }
+
+} else {
+  echo "You cannot upload";
+}
