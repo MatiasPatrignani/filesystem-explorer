e8d9631ac1fe969be88483d9f943db9b2ea3c860
Walber Melo <dev4wm@gmail.com>
2022-05-26 22:11:53 +0100

    feat(size): change file size format

--- modules/view_dir.php
+++ modules/view_dir.php
@@ -1,4 +1,28 @@
 <?php
+function sizeFormat($bytes){
+    $kb = 1024;
+    $mb = $kb * 1024;
+    $gb = $mb * 1024;
+    $tb = $gb * 1024;
+    
+    if (($bytes >= 0) && ($bytes < $kb)) {
+    return $bytes . ' B';
+    
+    } elseif (($bytes >= $kb) && ($bytes < $mb)) {
+    return ceil($bytes / $kb) . ' KB';
+    
+    } elseif (($bytes >= $mb) && ($bytes < $gb)) {
+    return ceil($bytes / $mb) . ' MB';
+    
+    } elseif (($bytes >= $gb) && ($bytes < $tb)) {
+    return ceil($bytes / $gb) . ' GB';
+    
+    } elseif ($bytes >= $tb) {
+    return ceil($bytes / $tb) . ' TB';
+    } else {
+    return $bytes . ' B';
+    }
+}
 
 
 function getDirContent($path){
@@ -11,13 +35,14 @@ function renderContents ($currentPath, $array) {
     foreach($array as $item){
         if($item !== '.' && $item !== '..') {
             if(is_dir("$currentPath/$item")) {
-                echo nl2br("<td class='dir-contents__folder border border-dark bi bi-folder col-5'> $item</div><br>");
-                echo "<td class='dirContents__folder col-sm text-center'></td>";
-                echo "<td class='dirContents__folder col-sm text-center'></td>";
-                echo "<td class='dirContents__folder col-sm text-center'></td>";
+                echo nl2br("<div class=' border border-dark bi bi-folder col-5'> $item</div><br>");
+                // echo "<td class='dirContents__folder col-sm text-center'></td>";
+                // echo "<td class='dirContents__folder col-sm text-center'></td>";
+                // echo "<td class='dirContents__folder col-sm text-center'></td>";
             } else if (is_file("$currentPath/$item")){
                 $fileName = pathinfo($item, PATHINFO_FILENAME);   // gets only filename, removing extension
                 $size = filesize("$currentPath/$item");
+                $size = sizeFormat($size);
                 $ext = pathinfo($item, PATHINFO_EXTENSION);
                 $mod = date("F d Y H:i:s.",filemtime("$currentPath/$item")); // issues with date
                 echo "<tr>";
