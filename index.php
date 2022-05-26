27a4c9895ef362a624b66e1791857b550da26fe2
MatiasPatrignani <m.patrignani56@gmail.com>
2022-05-26 18:01:20 +0200

    styles index

--- index.php
+++ index.php
@@ -12,28 +12,32 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
     <title>FileManager</title>
+    <script src="main.js" defer></script>
   </head>
   <body>
     <?php
       include './modules/layout/nav.php';
     ?>
     <!-- INPUT FILE -->
-    <header class="col-lg-6 col-sm-6 col-12">
+    <header class=" container col-8 mt-3">
       <form action="./modules/upload.php" method="POST" enctype="multipart/form-data" >
           <div class="input-group">
               <label class="input-group-btn">
                   <span class="btn btn-primary">
-                      Browse File <input type="file" name="add_file" style="display: none;" multiple>
+                      Browse File <input id="inputFile" type="file" name="add_file" style="display: none;" multiple>
                   </span>
               </label>
-              <input type="text" class="form-control" readonly>
-              <button type="submit" name="submit">Add file</button>
+              <!-- <input type="file" class="custom-file-input" id="inputGroupFile01"
+                  aria-describedby="inputGroupFileAddon01"> -->
+              <label class="custom-file-label form-control" readonly id="labelFile" for="file">Name file</label>
+              <!-- <input type="text" class="form-control" readonly> -->
+              <button class="btn btn-success" type="submit" name="submit">Add file</button>
           </div>
       </form>
     </header>
     <!-- TABLE OF FILES -->
-    <section class="container col-12">
-      <table class="table table-dark w-100">
+    <section class="container col-8 mt-3">
+      <table class="table table-striped w-100">
         <thead>
           <tr>
             <th class="col-sm" scope="col">Name</th>
